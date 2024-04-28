3ef6ed2a62feb38b9ba6299c69003268611386da
wangjielong <wangjielong@rjmart.cn>
2024-04-28 19:00:23 +0800

    area

---
+++ algorithm/src/main/java/com/yond/hot100/doublePointer/MaxArea.java
@@ -0,0 +1,12 @@
+package com.yond.hot100.doublePointer;
+
+public class MaxArea {
+
+    private int solution(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int area = 0;
+
+    }
+
+}
