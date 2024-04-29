f6caa15bb362607265097d0d7326a51d3acb77d7
wangjielong <wangjielong>
2024-04-29 08:39:08 +0800

    area

---
+++ algorithm/src/main/java/com/yond/hot100/doublePointer/ThreeSum.java
@@ -0,0 +1,28 @@
+package com.yond.hot100.doublePointer;
+
+import java.util.List;
+
+/**
+ * @Date 4/28/2024
+ * @Author yond
+ * @Description threeSum
+ */
+public class ThreeSum {
+
+    /**
+     * Example 1:
+     * Input: nums = [-1,0,1,2,-1,-4]
+     * Output: [[-1,-1,2],[-1,0,1]]
+     * Explanation:
+     * nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+     * nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+     * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+     * The distinct triplets are [-1,0,1] and [-1,-1,2].
+     * Notice that the order of the output and the order of the triplets does not matter.
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        int maxSize = 3;
+        
+    }
+
+}
