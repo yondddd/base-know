e9b65db32af02079f012e6481c172473137a3eb6
wangjielong <wangjielong>
2024-05-09 08:31:19 +0800

    point

--- algorithm/src/main/java/com/yond/hot100/doublePointer/ThreeSum.java
+++ algorithm/src/main/java/com/yond/hot100/doublePointer/ThreeSum.java
@@ -1,6 +1,6 @@
 package com.yond.hot100.doublePointer;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @Date 4/28/2024
@@ -21,8 +21,79 @@ public class ThreeSum {
      * Notice that the order of the output and the order of the triplets does not matter.
      */
     public List<List<Integer>> threeSum(int[] nums) {
-        int maxSize = 3;
-        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = nums[i];
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                if (left > i + 1) {
+                    if (nums[left] == nums[left - 1]) {
+                        left++;
+                        continue;
+                    }
+                }
+                if (right < nums.length - 1) {
+                    if (nums[right] == nums[right + 1]) {
+                        right--;
+                        continue;
+                    }
+                }
+                int size = nums[left] + nums[right];
+                int sum = size + target;
+                if (sum < 0) {
+                    left++;
+                } else {
+                    if (sum == 0) {
+                        // add
+                        List<Integer> list = new ArrayList<>();
+                        list.add(nums[left]);
+                        list.add(nums[right]);
+                        list.add(target);
+                        result.add(list);
+                    }
+                    right--;
+                }
+            }
+        }
+        return result;
     }
+
+
+    public List<List<Integer>> nSum(int[] nums) {
+        // 双指针+递归
+        Arrays.sort(nums);
+        return nSUmTarget(nums, 3, 0, 0);
+    }
+
+
+    private List<List<Integer>> nSUmTarget(int[] nums, int n, int index, int target) {
+        int arrayLength = nums.length;
+        if (arrayLength < 2 || n < 2) {
+            return Collections.emptyList();
+        }
+        if (n == 2) {
+            int left = index;
+            int right = arrayLength - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+
+                }
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        // 递归
+    }
+
+
 
 }
--- io/src/main/java/com/yond/Api.java
+++ io/src/main/java/com/yond/Api.java
@@ -1,10 +1,10 @@
 package com.yond;
 
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 /**
  * @Date 4/27/2024
@@ -18,20 +18,29 @@ public class Api {
     /**
      * 1. 为什么要用try包起来
      */
-    private static void readFile(String filePath) throws IOException {
+    private static byte[] readFile(String filePath) throws IOException {
         File file = new File(filePath);
+        ByteBuffer result = ByteBuffer.allocate((int) file.length());
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
             FileChannel channel = fileInputStream.getChannel();
-            fileInputStream.read();
-            channel.read();
+            while (channel.read(result) != -1) {
+                result.compact();
+            }
         }
+        return result.array();
+    }
+
+
+    private static void writeByte(String filePath, byte[] bytes) {
 
     }
 
 
     public static void main(String[] args) throws IOException {
         String path = "F:\\back\\resources\\io\\New Text Document.txt";
-        Api.readFile(path);
+        byte[] bytes = Api.readFile(path);
+        String s = new String(bytes, Charset.defaultCharset());
+        System.out.println(s);
     }
 
 }
